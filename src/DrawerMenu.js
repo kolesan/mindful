5d7a17065f931d4aeed4240bf76143d048dcc994
kolesan <kolesan90@gmail.com>
2018-10-31 22:31:39 +0200

    Add drawer menu

---
+++ src/DrawerMenu.js
@@ -0,0 +1,32 @@
+import './drawer_menu.css';
+
+const HIDDEN_DRAWER_CLASS = "drawer_menu__hidden";
+
+let drawer = document.querySelector(".drawer_menu");
+let drawerBtn = document.getElementById("menuBtn");
+drawerBtn.addEventListener("click", toggleDrawerState);
+let drawerOverlay = document.querySelector(".drawer_menu_overlay");
+drawerOverlay.addEventListener("click", toggleDrawerState);
+
+let shown = window.innerWidth > 1000;
+toggleDrawer();
+
+function toggleDrawerState(event) {
+  shown = !shown;
+  toggleDrawer();
+}
+function toggleDrawer() {
+  if (shown) {
+    showDrawer();
+  } else {
+    hideDrawer();
+  }
+}
+function showDrawer() {
+  drawer.classList.remove(HIDDEN_DRAWER_CLASS);
+  drawerOverlay.classList.remove(HIDDEN_DRAWER_CLASS);
+}
+function hideDrawer() {
+  drawer.classList.add(HIDDEN_DRAWER_CLASS);
+  drawerOverlay.classList.add(HIDDEN_DRAWER_CLASS);
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './style.css';
 import './rangeInput.css';
+
+import './DrawerMenu';
