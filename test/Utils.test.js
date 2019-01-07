bd5a1ab6a80dd4ebf1730111cbd508113c675ae2
kolesan <kolesan90@gmail.com>
2019-01-07 16:15:43 +0200

    Unit test for minmax utility fn

--- test/Utils.test.js
+++ test/Utils.test.js
@@ -8,3 +8,16 @@ test('assignDefinedProperties assigns only defined properties', () => {
   expect(a).toEqual({x: 10, y: 2, w: 30, h: 4, asd: ""});
 });
 
+test('can generate minmax function using `minmax`', () => {
+  let minmaxFn = Utils.minmax(-1, 20);
+
+  expect(minmaxFn(-100500)).toEqual(-1);
+  expect(minmaxFn(-2)).toEqual(-1);
+  expect(minmaxFn(-1)).toEqual(-1);
+  expect(minmaxFn(0)).toEqual(0);
+  expect(minmaxFn(5)).toEqual(5);
+  expect(minmaxFn(19)).toEqual(19);
+  expect(minmaxFn(20)).toEqual(20);
+  expect(minmaxFn(21)).toEqual(20);
+  expect(minmaxFn(250012)).toEqual(20);
+});
