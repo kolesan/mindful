59efe10973375a851d3cbe88118e236d78e0d420
kolesan <kolesan90@gmail.com>
2019-01-13 13:46:04 +0200

    Move jest config from package.json to jest.config.js. Use jest-summarizing-reporter custom reporter for jest.

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  verbose: true,
+  reporters: [
+    // "default",
+    ["jest-summarizing-reporter", {diffs: true}],
+    // "jest-summarizing-reporter"
+  ],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js"
+  }
+};
