4dab8bac81b5d3eb46db4db698623f8bc1b5d9ca
kolesan <kolesan90@gmail.com>
2019-01-14 21:42:41 +0200

    Change jest config

--- jest.config.js
+++ jest.config.js
@@ -2,8 +2,8 @@ module.exports = {
   verbose: true,
   reporters: [
     "default",
-    ["jest-summarizing-reporter", {diffs: true}],
-    // "jest-summarizing-reporter"
+    // ["jest-summarizing-reporter", {diffs: true}],
+    "jest-summarizing-reporter"
   ],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js",
