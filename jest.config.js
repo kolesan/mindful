f41a23e25125d48e940c9c6cbc754b911a647c27
kolesan <kolesan90@gmail.com>
2019-01-15 17:39:12 +0200

    Ignore TestUtils during test coverage calculations

--- jest.config.js
+++ jest.config.js
@@ -9,5 +9,8 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/test/__mocks__/styleMock.js"
   },
-  setupTestFrameworkScriptFile: "jest-extended"
+  setupTestFrameworkScriptFile: "jest-extended",
+  coveragePathIgnorePatterns : [
+    "TestUtils"
+  ]
 };
