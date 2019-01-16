6abb8c1256db1787fe4c6ee277bbea7a07ed9af7
kolesan <kolesan90@gmail.com>
2019-01-16 17:32:05 +0200

    Add some Timer integration tests

--- test/timer_screen/timer/Timer.integration.test.js
+++ test/timer_screen/timer/Timer.integration.test.js
@@ -1,37 +1,100 @@
-import { log, logo } from "../../TestUtils";
-import { programBuilder } from "../../../src/dev/DevUtils";
+import { log, logo, nowKeeper } from "../../TestUtils";
 
 import { newTimer } from '../../../src/timer_screen/timer/Timer';
 import timeKeeper from '../../../src/timer_screen/timer/TimeKeeper';
 import eventTraversal from '../../../src/timer_screen/timer/EventTraversal';
 import iterableTimerProgram from '../../../src/timer_screen/timer/IterableTimerProgram';
 
+let noop = jest.fn();
 let prepCb = jest.fn();
 let pose12Cb = jest.fn();
 let pose22Cb = jest.fn();
 let changePoseCb = jest.fn();
 let chillCb = jest.fn();
 
+jest.useFakeTimers();
+let now = nowKeeper();
+function advanceTime(t) {
+  jest.advanceTimersByTime(t);
+  now.advance(t);
+}
+
 afterEach(() => {
-  jest.resetAllMocks();
+  //jest.resetAllMocks() for some reason prevents setInterval from happening in TimeKeeper
+  //so we need to reset all mocks by hand
+  noop.mockReset();
+  prepCb.mockReset();
+  pose12Cb.mockReset();
+  pose22Cb.mockReset();
+  changePoseCb.mockReset();
+  chillCb.mockReset();
+
+  now = nowKeeper();
 });
 
 let program = {
-  mainEvent: programBuilder( `Mindful Yoga` )
-                     .event( `Preparation`, 30000, prepCb ).end()
-                     .loop(50)
-                       .event( `Hold pose {i}`, 60000 )
-                         .event( `1/2`, 30000, pose12Cb ).end()
-                         .event( `2/2`, 30000, pose22Cb ).end()
-                       .end()
-                       .event( `Change pose`, 10000, changePoseCb ).end()
-                     .end()
-                     .event( `Chill`, 70000, chillCb )
-                     .build()
+  mainEvent: {
+    element: "event",
+    name: "Mindful Yoga",
+    duration: 310000,
+    callback: noop,
+    children: [
+      {
+        element: "event",
+        name: "Preparation",
+        duration: 30000,
+        callback: prepCb,
+        children: []
+      },
+      {
+        element: "loop",
+        iterations: 3,
+        duration: 210000,
+        children: [
+          {
+            element: "event",
+            name: "Hold pose {i}",
+            duration: 60000,
+            callback: noop,
+            children: [
+              {
+                element: "event",
+                name: "1/2",
+                duration: 30000,
+                callback: pose12Cb,
+                children: []
+              },
+              {
+                element: "event",
+                name: "2/2",
+                duration: 30000,
+                callback: pose22Cb,
+                children: []
+              }
+            ]
+          },
+          {
+            element: "event",
+            name: "Change pose",
+            duration: 10000,
+            callback: changePoseCb,
+            children: []
+          }
+        ]
+      },
+      {
+        element: "event",
+        name: "Chill",
+        duration: 70000,
+        callback: chillCb,
+        children: []
+      }
+    ]
+  }
 };
 
 it('can be created', () => {
-  let timer = newTimer(timeKeeper(1000), eventTraversal(iterableTimerProgram(program)));
+  let timer = newTimer(timeKeeper(1000, now), eventTraversal(iterableTimerProgram(program)));
 
   expect(timer.running).toEqual(false);
   expect(timer.paused).toEqual(false);
@@ -58,4 +121,79 @@ it('can be created', () => {
     "init", "start", "pause", "stop", "seek",
     "currentEvents"
   ]);
+});
+
+it('state change from start, pause and stop is reflected through running, paused and stopped', () => {
+  let timer = newTimer(timeKeeper(1000, now), eventTraversal(iterableTimerProgram(program)));
+
+  timer.start();
+  advanceTime(100);
+
+  expect(timer.running).toEqual(true);
+  expect(timer.paused).toEqual(false);
+  expect(timer.stopped).toEqual(false);
+
+  timer.pause();
+  advanceTime(100);
+
+  expect(timer.running).toEqual(false);
+  expect(timer.paused).toEqual(true);
+  expect(timer.stopped).toEqual(false);
+
+  timer.stop();
+  advanceTime(100);
+
+  expect(timer.running).toEqual(false);
+  expect(timer.paused).toEqual(false);
+  expect(timer.stopped).toEqual(true);
+});
+
+it('onStart, onPause and onFinish callbacks are called on start, pause and stop', () => {
+  let timer = newTimer(timeKeeper(1000, now), eventTraversal(iterableTimerProgram(program)));
+  let onStartCb = jest.fn();
+  let onPauseCb = jest.fn();
+  let onFinishCb = jest.fn();
+  timer.onStart(onStartCb);
+  timer.onPause(onPauseCb);
+  timer.onFinish(onFinishCb);
+
+  timer.start();
+  advanceTime(100);
+
+  timer.pause();
+  advanceTime(100);
+
+  timer.stop();
+  advanceTime(100);
+
+  expect(onStartCb).toHaveBeenCalledTimes(1);
+  expect(onStartCb).toHaveBeenCalledBefore(onPauseCb);
+  expect(onStartCb).toHaveBeenCalledBefore(onFinishCb);
+
+  expect(onPauseCb).toHaveBeenCalledTimes(1);
+  expect(onPauseCb).toHaveBeenCalledAfter(onStartCb);
+  expect(onPauseCb).toHaveBeenCalledBefore(onFinishCb);
+
+  expect(onFinishCb).toHaveBeenCalledTimes(1);
+  expect(onFinishCb).toHaveBeenCalledAfter(onStartCb);
+  expect(onFinishCb).toHaveBeenCalledAfter(onPauseCb);
+});
+
+it('onFinish callback is called after the last event occurs and the timer is considered finished', () => {
+  let timer = newTimer(timeKeeper(1000, now), eventTraversal(iterableTimerProgram(program)));
+  let onFinishCb = jest.fn();
+  timer.onFinish(onFinishCb);
+
+  timer.start();
+  advanceTime(310000+1000); //Program duration + 1s
+
+  expect(timer.running).toEqual(false);
+  expect(timer.paused).toEqual(false);
+  expect(timer.stopped).toEqual(true);
+
+  expect(onFinishCb).toHaveBeenCalledTimes(1);
+});
+
+it('', () => {
+
 });
