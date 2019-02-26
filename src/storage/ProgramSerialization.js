8db31355bdda8148c623d4c8501aa53153a27ce0
kolesan <kolesan90@gmail.com>
2019-02-26 15:40:43 +0200

    Extract Program Element Serialization logic to separate module and access it through ConverterRegistry in ProgramSerialization

--- src/storage/ProgramSerialization.js
+++ src/storage/ProgramSerialization.js
@@ -1,40 +1,16 @@
 import { log } from "../utils/Logging";
 
-import { mapTree } from "../utils/TreeUtils";
-import { callbackDictionary } from '../EventCallbacks';
-import { optional } from '../utils/FunctionalUtils';
 import programInstance from '../program_model/Program';
-import programEvent from '../program_model/ProgramEvent';
-import programLoop from '../program_model/ProgramLoop';
-import ToolNames from "../edit_screen/tools/ToolNames";
+import converterRegistry, { Converters } from "../program_model_converters/ConverterRegistry";
 
-export default newInstance(callbackDictionary);
+export default newInstance(converterRegistry.get(Converters.localStorage));
 
-export function newInstance(callbackDictionary) {
-  let dictionaryEntriesArray = [...callbackDictionary.entries()];
+export function newInstance(elementConverter) {
   return Object.freeze({
     serialize(program) {
       let { mainEvent, ...serializedProgram } = program;
-
-      serializedProgram.mainEvent = mapTree(mainEvent, serializeElement);
-
+      serializedProgram.mainEvent = elementConverter.serialize(mainEvent);
       return serializedProgram;
-
-      function serializeElement(element) {
-        let { callback, ...serializedElement } = element;
-        if (callback) {
-          serializedElement.callback = serializeCallback(callback);
-        }
-        return serializedElement;
-      }
-
-      function serializeCallback(callback) {
-        return optional(dictionaryEntriesArray.find(([name, fn]) => fn === callback))
-          .ifEmpty(() => {
-            throw Error(`Provided callback dictionary does not contain function '${callback}'`)
-          })
-          .value[0];
-      }
     },
     deserialize(program) {
       return programInstance({
@@ -43,50 +19,8 @@ export function newInstance(callbackDictionary) {
         icon: program.icon,
         description: program.description,
         timesOpened: program.timesOpened,
-        mainEvent: deserializeElements(program.mainEvent)
+        mainEvent: elementConverter.deserialize(program.mainEvent)
       });
-
-      function deserializeElements(mainEvent) {
-        return mapTree(mainEvent, node => {
-          let element = deserializeElement(node);
-          element.children = node.children || [];
-          return element;
-        })
-      }
-
-      function deserializeElement(element) {
-        switch(element.element) {
-          case ToolNames.loop:
-            return deserializeLoop(element);
-          case ToolNames.event:
-            return deserializeEvent(element);
-          default:
-            throw Error(`Unknown program element ${element.element} encountered`);
-        }
-      }
-
-      function deserializeLoop(loop) {
-        return programLoop({
-          iterations: loop.iterations,
-          duration: loop.duration
-        });
-      }
-
-      function deserializeEvent(event) {
-        return programEvent({
-          name: event.name,
-          callback: deserializeCallback(event.callback),
-          duration: event.duration
-        });
-      }
-
-      function deserializeCallback(callback) {
-        return optional(callbackDictionary.get(callback))
-          .ifEmpty(() => {
-            throw Error(`Provided callback dictionary does not contain function '${callback}'`);
-          })
-          .value;
-      }
     }
   })
 
--- test/storage/ProgramSerialization.test.js
+++ test/storage/ProgramSerialization.test.js
@@ -1,140 +1,32 @@
 import "@babel/polyfill";
 import { newInstance as programSerialization } from '../../src/storage/ProgramSerialization';
 
-let a = jest.fn();
-let b = jest.fn();
-let c = jest.fn();
+let converter = {
+  serialize: jest.fn( () => ({}) ),
+  deserialize: jest.fn( () => ({}) )
+};
 
-let callbacks = new Map()
-  .set("a", a)
-  .set("b", b)
-  .set("c", c);
+let service = programSerialization(converter);
 
 let serializedProgram = {
-  mainEvent: {
-    element: "event",
-    name: "Mindful Yoga",
-    duration: 310000,
-    callback: "a",
-    children: [
-      {
-        element: "event",
-        name: "Preparation",
-        duration: 30000,
-        callback: "b",
-        children: []
-      },
-      {
-        element: "loop",
-        iterations: 3,
-        duration: 210000,
-        children: [
-          {
-            element: "event",
-            name: "Hold pose {i}",
-            duration: 60000,
-            callback: "b",
-            children: [
-              {
-                element: "event",
-                name: "1/2",
-                duration: 30000,
-                callback: "c",
-                children: []
-              },
-              {
-                element: "event",
-                name: "2/2",
-                duration: 30000,
-                callback: "c",
-                children: []
-              }
-            ]
-          },
-          {
-            element: "event",
-            name: "Change pose",
-            duration: 10000,
-            callback: "b",
-            children: []
-          }
-        ]
-      },
-      {
-        element: "event",
-        name: "Chill",
-        duration: 70000,
-        callback: "a",
-        children: []
-      }
-    ]
-  }
+  id: "MyProgram",
+  title: "My Program",
+  icon: "fas fa-bender",
+  description: "Some program",
+  timesOpened: 25,
+  mainEvent: expect.anything()
 };
 
 let deserializedProgram = {
-  mainEvent: {
-    element: "event",
-    name: "Mindful Yoga",
-    duration: 310000,
-    callback: a,
-    children: [
-      {
-        element: "event",
-        name: "Preparation",
-        duration: 30000,
-        callback: b,
-        children: []
-      },
-      {
-        element: "loop",
-        iterations: 3,
-        duration: 210000,
-        children: [
-          {
-            element: "event",
-            name: "Hold pose {i}",
-            duration: 60000,
-            callback: b,
-            children: [
-              {
-                element: "event",
-                name: "1/2",
-                duration: 30000,
-                callback: c,
-                children: []
-              },
-              {
-                element: "event",
-                name: "2/2",
-                duration: 30000,
-                callback: c,
-                children: []
-              }
-            ]
-          },
-          {
-            element: "event",
-            name: "Change pose",
-            duration: 10000,
-            callback: b,
-            children: []
-          }
-        ]
-      },
-      {
-        element: "event",
-        name: "Chill",
-        duration: 70000,
-        callback: a,
-        children: []
-      }
-    ]
-  }
+  id: "MyProgram",
+  title: "My Program",
+  icon: "fas fa-bender",
+  description: "Some program",
+  timesOpened: 25,
+  mainEvent: expect.anything()
 };
 
 it('can be created', () => {
-  let service = programSerialization(callbacks);
-
   expect(service.serialize).toBeFunction();
   expect(service.deserialize).toBeFunction();
 
@@ -144,61 +36,37 @@ it('can be created', () => {
 });
 
 it('can deserialize a program', () => {
-  let service = programSerialization(callbacks);
-
-  expect(service.deserialize(serializedProgram)).toEqual(deserializedProgram)
+  expect(service.deserialize(serializedProgram)).toEqual(deserializedProgram);
+  expect(converter.deserialize).toHaveBeenCalledTimes(1);
 });
 
 it('can serialize a program', () => {
-  let service = programSerialization(callbacks);
-
-  expect(service.serialize(deserializedProgram)).toEqual(serializedProgram)
+  expect(service.serialize(deserializedProgram)).toEqual(serializedProgram);
+  expect(converter.serialize).toHaveBeenCalledTimes(1);
 });
 
+
+
 it('deserialize returns new instance of program', () => {
-  let service = programSerialization(callbacks);
-
   let deserialized = service.deserialize(serializedProgram);
-  deserialized.mainEvent.name = "DefinitelySomethingNew";
-  deserialized.mainEvent.children[0].name = "AlsoDefinitelySomethingNew";
+  deserialized.title = "DefinitelySomethingNew";
+  deserialized.timesOpened = 100500;
 
-  expect(deserialized.mainEvent.name).toEqual("DefinitelySomethingNew");
-  expect(serializedProgram.mainEvent.name).toEqual("Mindful Yoga");
+  expect(deserialized.title).toEqual("DefinitelySomethingNew");
+  expect(serializedProgram.title).toEqual("My Program");
 
-  expect(deserialized.mainEvent.children[0].name).toEqual("AlsoDefinitelySomethingNew");
-  expect(serializedProgram.mainEvent.children[0].name).toEqual("Preparation");
+  expect(deserialized.timesOpened).toEqual(100500);
+  expect(serializedProgram.timesOpened).toEqual(25);
 });
 
 it('serialize returns new instance of program', () => {
-  let service = programSerialization(callbacks);
-
   let serialized = service.serialize(deserializedProgram);
-  serialized.mainEvent.name = "DefinitelySomethingNew";
-  serialized.mainEvent.children[0].name = "AlsoDefinitelySomethingNew";
+  serialized.title = "DefinitelySomethingNew";
+  serialized.timesOpened = 100500;
 
-  expect(serialized.mainEvent.name).toEqual("DefinitelySomethingNew");
-  expect(deserializedProgram.mainEvent.name).toEqual("Mindful Yoga");
+  expect(serialized.title).toEqual("DefinitelySomethingNew");
+  expect(deserializedProgram.title).toEqual("My Program");
 
-  expect(serialized.mainEvent.children[0].name).toEqual("AlsoDefinitelySomethingNew");
-  expect(deserializedProgram.mainEvent.children[0].name).toEqual("Preparation");
-});
-
-it('during serialization throws an error if callback dictionary does not contain needed function', () => {
-  let service = programSerialization(callbacks);
-
-  let deserializedClone = service.deserialize(serializedProgram);
-  deserializedClone.mainEvent.callback = function NoSuchFunction() {};
-
-  expect(() => service.serialize(deserializedClone))
-    .toThrowWithMessage(Error, /Provided callback dictionary does not contain.*NoSuchFunction/);
-});
-
-it('during deserialization throws an error if callback dictionary does not contain needed function', () => {
-  let service = programSerialization(callbacks);
-
-  let serializedClone = service.serialize(deserializedProgram);
-  serializedClone.mainEvent.callback = "NoSuchFunction";
-
-  expect(() => service.deserialize(serializedClone))
-    .toThrowWithMessage(Error, /Provided callback dictionary does not contain.*NoSuchFunction/);
+  expect(serialized.timesOpened).toEqual(100500);
+  expect(deserializedProgram.timesOpened).toEqual(25);
 });
